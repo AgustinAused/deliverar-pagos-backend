7319539688bf17bc1d7194c499875bd5fca4357d
Alejandra Joaquin <alejandraejoaquin@gmail.com>
2025-05-20 15:57:06 -0300

    adding admin creation

---
+++ src/main/java/com/deliverar/pagos/infrastructure/bootstrap/AdminBootstrapRunner.java
@@ -0,0 +1,42 @@
+package com.deliverar.pagos.infrastructure.bootstrap;
+
+import com.deliverar.pagos.domain.entities.Role;
+import com.deliverar.pagos.domain.entities.User;
+import com.deliverar.pagos.domain.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AdminBootstrapRunner implements CommandLineRunner {
+
+    private final UserRepository userRepo;
+    private final PasswordEncoder encoder;
+    @Value("${app.bootstrap.admin.email}")
+    private String adminEmail;
+    @Value("${app.bootstrap.admin.name}")
+    private String name;
+    @Value("${app.bootstrap.admin.password}")
+    private String adminPass;
+
+    public AdminBootstrapRunner(UserRepository userRepo, PasswordEncoder encoder) {
+        this.userRepo = userRepo;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (userRepo.count() == 0) {
+            User admin = new User();
+            admin.setName(adminEmail);
+            admin.setEmail(adminEmail);
+            admin.setPasswordHash(encoder.encode(adminPass));
+            admin.setRole(Role.ADMIN);
+            userRepo.save(admin);
+            log.info("Admin created");
+        }
+    }
+}
