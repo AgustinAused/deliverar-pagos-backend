cfbc6ff7cfd8870f6b365f67e68ce09380acefc6
Alejandra Joaquin <alejandraejoaquin@gmail.com>
2025-05-20 21:19:11 -0300

    creating get user by id endpoint

--- src/main/java/com/deliverar/pagos/application/controllers/UserController.java
+++ src/main/java/com/deliverar/pagos/application/controllers/UserController.java
@@ -3,13 +3,17 @@ package com.deliverar.pagos.application.controllers;
 
 import com.deliverar.pagos.domain.dtos.CreateUserRequest;
 import com.deliverar.pagos.domain.dtos.CreateUserResponse;
+import com.deliverar.pagos.domain.dtos.UserDto;
 import com.deliverar.pagos.domain.entities.User;
 import com.deliverar.pagos.domain.usecases.user.CreateUser;
+import com.deliverar.pagos.domain.usecases.user.GetUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequestMapping("/api/user")
 @RestController
@@ -18,11 +22,26 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final CreateUser createUser;
     private final PasswordEncoder passwordEncoder;
+    private final GetUser getUser;
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public CreateUserResponse create(@RequestBody CreateUserRequest request) {
         User user = createUser.create(request.getName(), request.getEmail().toLowerCase(), passwordEncoder.encode(request.getPassword()), request.getRole());
         return CreateUserResponse.builder().id(user.getId()).build();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto get(@PathVariable UUID id) {
+        User user = getUser.get(id);
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .role(user.getRole())
+                .build();
     }
 }
