3df5ffeca182a81ae7f81b673802cb3646788e94
Agustin Aused <agus.aused@gmail.com>
2025-05-21 23:43:50 -0300

    fix: update CORS configuration to allow specific frontend origin

--- src/main/java/com/deliverar/pagos/application/configuration/SecurityConfig.java
+++ src/main/java/com/deliverar/pagos/application/configuration/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOriginPatterns(List.of("aaafront.blockchain.deliver.ar")); // Permite cualquier origen
+        cfg.setAllowedOriginPatterns(List.of("front.blockchain.deliver.ar")); // Permite cualquier origen
         cfg.setAllowedMethods(List.of(""));        // Permite cualquier método
         cfg.setAllowedHeaders(List.of("*"));        // Permite cualquier header
         cfg.setAllowCredentials(true);
